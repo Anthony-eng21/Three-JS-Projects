d31819eb09d9db06d02f72f873f68bfb311cf926
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-08-26 20:08:29 -0700

    got to HemisphereLight THREE class

--- Lights_yer/src/script.js
+++ Lights_yer/src/script.js
@@ -18,15 +18,50 @@ const scene = new THREE.Scene();
 
 /**
  * Lights
+ *
+ * The AmbientLight applies omnidirectional lighting on all geometries of the scene.
+ * @params The first parameter is the color and the second parameter is the intensity
+ * As for the materials, you can set the properties directly when instantiating or you can change them after:
+ * - If all you have is an AmbientLight you'll have the same effect as for a MeshBasicMaterial because all faces of the geometries will be lit equally.
+ * Ambient Light:
+ * LIGHT BOUNCING is NOT supported in Three.js. use a dim AmbientLight to fake this light bounce.
+ * @params The first parameter is the color and the second is the intensity. (default color hex)
+ * @params The second parameter is the intensity of the "rays". (default float max 1)
+ *
+ * The Directional Light:
+ * has a sun-like effect as if the sun rays were traveling in parallel.
+ * @params The first parameter is the color.
+ * @params The second parameter is the intensity:
+ * By default, the light seems to come from above.
+ * To change this, move the whole light by using the position
+ * property like if it were a normal object.
+ * The distance of the light doesn't matter for now.
+ * The rays come from an infinite space and travel
+ * in parallel to the infinite opposite.
+ * - CODE:
+ * const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+ * scene.add(directionalLight);
+ * directionalLight.position.set(1, 0.25, 0);
+ *
+ * HemisphereLight
+ * The HemisphereLight is similar to the AmbientLight but with a different
+ * color from the sky than the color coming from the ground. Faces facing
+ * the sky will be lit by one color while another color will lit faces facing the ground
+ * @params The first parameter is the color corresponding to the sky color
+ * @params the second parameter is the groundColor.
+ * @params the third parameter is the intensity:
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+//CODE:
+const ambientLight = new THREE.AmbientLight();
+ambientLight.color = new THREE.Color(0xffffff);
+ambientLight.intensity = 0.5;
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+scene.add(hemisphereLight);
+
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
 
 /**
  * Objects
