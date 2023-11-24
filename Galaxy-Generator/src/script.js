9ad802ce38c8fb16775d5f89e4387395bcce726e
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-11-24 15:35:50 -0800

    start branches

--- Galaxy-Generator/src/script.js
+++ Galaxy-Generator/src/script.js
@@ -1,29 +1,114 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
+const gui = new GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Each time we call the generate function, we will remove the previous galaxy 
+// Each time we call the generateGalaxy function, we will remove the previous galaxy
 // (if there is one)
-// and create a new one. We can call that function immediately
+// then create a new one. then call that function immediately
 
+// Object to hold all the parameters for our galaxy
+// add each parameter to our lil-gui
 const parameters = {};
-parameters.count = 1000;
+parameters.count = 100000;
+parameters.size = 0.01;
 
-const generateGalaxy = () => {
+// Each star will be positioned accordingly to that radius.
+// If the radius is 5, the stars will be positioned at a distance from 0 to 5
+parameters.radius = 5;
 
-}
+//spin galaxies always seem to have 2 branches, but we can have more honestly 
+
+let geometry = null;
+let material = null;
+let points = null;
+
+//GEN GAL fn
+var generateGalaxy = () => {
+  //destroy the old galaxy
+
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
+  /**
+   * Geometry
+   */
+  geometry = new THREE.BufferGeometry();
+
+  //array to hold our vertices for our particle 'points' (rule of 3 vertices)
+  const positions = new Float32Array(parameters.count * 3);
+
+  // in out loop we create our particles for our geometry
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    const radius = Math.random() * parameters.radius;
+
+    positions[i3] = radius; //x vertice Float32Array[0]
+    positions[i3 + 1] = 0; // y vertice Float32Array[1]
+    positions[i3 + 2] = 0; // x vertice Float32Array[2]
+
+    // positions[i3] = (Math.random() - 0.5) * 3; //x vertice Float32Array[0]
+    // positions[i3 + 1] = (Math.random() - 0.5) * 3; // y vertice Float32Array[1]
+    // positions[i3 + 2] = (Math.random() - 0.5) * 3; // x vertice Float32Array[2]
+  }
+  //add our mutated particles to our position attribute on our geometry
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  /**
+   * Materials
+   *
+   */
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  });
+
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+//end GEN GAL fn
+
+//start params for Galaxy obj for lil gui
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(1000000)
+  .step(100)
+  // Pass a function to be called after this controller has been modified and loses focus.
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  // Pass a function to be called after this controller has been modified and loses focus.
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+
+// end params for Galaxy obj for lil gui
 
 generateGalaxy();
 
@@ -31,65 +116,68 @@ generateGalaxy();
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
