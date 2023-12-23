1b694d77f5764f50cc9266b351c397bab13fbedd
Anthony-eng21 <102385518+Anthony-eng21@users.noreply.github.com>
2023-12-22 22:03:32 -0800

    DONE

--- scroll-based-animation/src/script.js
+++ scroll-based-animation/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Debug
@@ -14,6 +15,7 @@ const parameters = {
 // update the material accordingly without this it doesn't work
 gui.addColor(parameters, "materialColor").onChange(() => {
   material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
 });
 
 /**
@@ -72,7 +74,7 @@ scene.add(mesh1, mesh2, mesh3);
 // mesh3.scale.set(0.5, 0.5, 0.5)
 
 // makes sure that each object is far enough away from the other on the y axis
-const objectDistance = 4; //for positoning our geometries
+const objectDistance = 5; //for positoning our geometries
 // The values must be negative so that the objects go down:
 mesh1.position.y = -objectDistance * 0;
 mesh2.position.y = -objectDistance * 1;
@@ -86,9 +88,57 @@ scene.add(mesh1, mesh2, mesh3);
 
 const sectionMeshes = [mesh1, mesh2, mesh3];
 
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
+
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectDistance * 0.5 -
+    Math.random() * objectDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
 let scrollY = window.scrollY;
+let currentSection = 0;
+
 window.addEventListener("scroll", () => {
   scrollY = window.scrollY;
+
+  const newSection = Math.round(scrollY / sizes.height); // calculates our next section
+
+  console.log(newSection);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
+  }
 
   // console.log(scrollY)
 });
@@ -199,8 +249,8 @@ const tick = () => {
   //permanent rotation
 
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.12;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // Render
